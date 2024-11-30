bb2210dd25c2abada53682825959d20ce36d11e8
Jenn Junod <jenn.junod@gmail.com>
2024-11-30 10:48:33 -0700

    about site updates

--- src/config.mjs
+++ src/config.mjs
@@ -1,21 +1,21 @@
 export default {
-    title: "Jenn Junod",
-    titleSuffix: " | Full Stack Developer & Developer Advocate",
-    description: "I specialize in making technology human — enabling developers to understand, adopt, and adore technical products.",
-    defaultOgImage: "https://opengraph.b-cdn.net/production/images/472241ee-3726-4492-b73b-976b50e8b0f1.jpg?token=33NiEpv5gmrrJBx8qHOX-pQ3Z9PA1Y7acKgzrGZXDRE&height=438&width=491&expires=33268139322",
+    title: "Shit2TalkAbout",
+    titleSuffix: "",
+    description: "Solve your curiosity and learn from others conversations",
+    defaultOgImage:"https://opengraph.b-cdn.net/production/images/27abbd00-070b-41a8-ac46-ffb3828e814e.png?token=-KWeYREtv29NZD5RgnectRQkCq4iY58nyOGhcjs-RM0&height=716&width=915&expires=33268315468",
         latestPosts: 3, 
     postsPerPage: 3,
     url: "https://jennjunod.dev",
     social: {
         'dribbble': false,
-        'facebook': false,
-        'github': 'https://github.com/jennjunod',
+        'facebook': 'https://www.facebook.com/shit2talkabout',
+        'github': false,
         'instagram': false,
         'mastodon': false,
         'pinterest': false,
         'skype': false,
-        'twitter': 'https://twitter.com/jennjunod',
-        'blusky': 'https://bsky.app/profile/jennjunod.bsky.social',
-        'linkedin': 'https://www.linkedin.com/in/jennjunod/'
+        'twitter': 'https://twitter.com/shit2talkabout',
+        'blusky': false,
+        'linkedin': 'https://www.linkedin.com/company/shit2talkabout'
     }
  }
